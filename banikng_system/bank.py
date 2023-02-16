da3a1ac2e13f0a7654f13729eff24231fba51dfc
ArtavazdGevorgyan <ag15012003@gmail.com>
2023-02-16 18:53:45 +0400

    Update bank.py

--- banikng_system/bank.py
+++ banikng_system/bank.py
@@ -1,17 +1,18 @@
 from user import User
+
+
 class Bank:
-    def __init__(self, amount:int) -> None:
+    def __init__(self, amount: int) -> None:
         self.__amount = amount
 
-    def deposit(self, amount:int, user:User):
+    def deposit(self, amount: int, user: User):
         if user.get_amount() > amount:
             raise ValueError()
         else:
             self.__amount += amount
             user.set_amount(user.get_amount - amount)
-        
-        
-    def withdraw(self,amount:int,user:User):
+
+    def withdraw(self, amount: int, user: User):
         if user.get_amount() > amount:
             self.__amount += amount
             user.set_amount(user.get_amount + amount)
